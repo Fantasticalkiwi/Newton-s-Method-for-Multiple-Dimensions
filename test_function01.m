135f53ae82715ecf5f3de687945c034e1efc3dc2
Kai-Xing Yue <79027796+Fantasticalkiwi@users.noreply.github.com>
2026-09-21 09:49:30 -0400

    add file

---
+++ test_function01.m
@@ -0,0 +1,7 @@
+%the function name and input/output variable names
+%are just what I chose, you can use whatever names you'd like
+function [f_val,J] = test_function01(X)
+%your code here
+f_val = zeros(3);
+f_val(end) = X
+end
