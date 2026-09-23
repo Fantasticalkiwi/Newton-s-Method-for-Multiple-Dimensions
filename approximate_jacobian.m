061aab3973ea4d3bcee1321fb47f8146430c966e
Kai-Xing Yue <79027796+Fantasticalkiwi@users.noreply.github.com>
2026-09-23 12:28:07 -0400

    add functions

---
+++ approximate_jacobian.m
@@ -0,0 +1,10 @@
+Implementation of finite difference approximation
+%for Jacobian of multidimensional function
+%INPUTS:
+%fun: the mathetmatical function we want to differentiate
+%X: the input value of fun that we want to compute the derivative at
+%OUTPUTS:
+%J: approximation of Jacobian of fun at X
+function J = approximate_jacobian(fun,X)
+%your code here
+end
